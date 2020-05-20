73624811209de2906689c83aa9bae4abeff2e053
Pedro-Luna <59538557+Pedro-Luna@users.noreply.github.com>
2020-05-20 16:37:24 -0600

    Add files via upload

---
+++ conversion_numeros/src/conversion_numeros/Conversion_numeros.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conversion_numeros;
+
+
+
+
+
+/**
+ *
+ * @author pedro
+ */
+public class Conversion_numeros {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ventana x = new ventana();
+       x.setVisible(true);
+       x.setLocationRelativeTo(null);
+    }
+
+
+
+}
